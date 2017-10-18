183ef9767b40d36f1efd2da966dd37a341d346f4
OniriiWu <30306017+OniriiWu@users.noreply.github.com>
2017-10-18 14:47:05 +0800

    第二次修改
    上传第一个web application

---
+++ WebApplication15/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication15.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string msg = "hello world!";
+            ViewBag.Msg = msg;
+            ViewData["Msg"] = msg;
+            TempData["Msg"] = msg;
+            return View();
+        }
+
+    public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
